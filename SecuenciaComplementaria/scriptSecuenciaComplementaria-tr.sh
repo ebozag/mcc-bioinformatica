528c3e1a2d866cf8146ef57a0d16fe6fcb85f077
Edwin Boza <eboza@espol.edu.ec>
2015-10-24 10:31:57 -0500

    Agrego descartar la primera línea.

--- SecuenciaComplementaria/scriptSecuenciaComplementaria-tr.sh
+++ SecuenciaComplementaria/scriptSecuenciaComplementaria-tr.sh
@@ -4,6 +4,6 @@ filename=$1
 
 echo "Procesando secuencia en el archivo: $filename"
 
-cat $filename | tr "ATGC" "TACG" > $filename-comp2
+tail -n +2 $filename | tr "ATGC" "TACG" > $filename-comp2
 
 echo "El archivo con la secuencia complementaria resultante es: $filename-comp2"
