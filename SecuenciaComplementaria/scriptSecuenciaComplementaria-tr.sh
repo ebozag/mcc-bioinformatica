1943135af9c61321c9bc30e2b165a6f516f037e7
Edwin Boza <eboza@espol.edu.ec>
2015-10-24 10:33:46 -0500

    Agrego descartar las líneas con el símbolo >.

--- SecuenciaComplementaria/scriptSecuenciaComplementaria-tr.sh
+++ SecuenciaComplementaria/scriptSecuenciaComplementaria-tr.sh
@@ -4,6 +4,6 @@ filename=$1
 
 echo "Procesando secuencia en el archivo: $filename"
 
-tail -n +2 $filename | tr "ATGC" "TACG" > $filename-comp2
+grep -v ">" $filename | tr "ATGC" "TACG" > $filename-comp2
 
 echo "El archivo con la secuencia complementaria resultante es: $filename-comp2"
